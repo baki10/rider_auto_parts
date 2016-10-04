0929f9893d1bfb8a782afb98526a71a22f4fea3c
Ilmir Bakirov <ilmirbakirov@gmail.com>
2016-10-04 13:25:57 +0300

    annotation component scan

---
+++ src/main/java/com/bakigoal/spring/route/FtpToJmsAnnotationRoute.java
@@ -0,0 +1,19 @@
+package com.bakigoal.spring.route;
+
+import com.bakigoal.DownloadLoggerProcessor;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by ilmir on 04.10.16.
+ */
+@Component
+public class FtpToJmsAnnotationRoute extends RouteBuilder {
+
+  private static final String FTP_URI = "file:src/data?noop=true";
+  private static final String JMS_URI = "jms:queue:incomingOrders";
+
+  public void configure() throws Exception {
+    from(FTP_URI).process(new DownloadLoggerProcessor()).to(JMS_URI);
+  }
+}
