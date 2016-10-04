6be5d4207d2b44ec6bc2d6b99e3787cc04b91e79
Ilmir Bakirov <ilmirbakirov@gmail.com>
2016-10-04 13:31:48 +0300

    annotation component scan

--- src/main/java/com/bakigoal/spring/route/FtpToJmsAnnotationRoute.java
+++ src/main/java/com/bakigoal/spring/route/FtpToJmsAnnotationRoute.java
@@ -2,13 +2,14 @@ package com.bakigoal.spring.route;
 
 import com.bakigoal.DownloadLoggerProcessor;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.spring.SpringRouteBuilder;
 import org.springframework.stereotype.Component;
 
 /**
  * Created by ilmir on 04.10.16.
  */
 @Component
-public class FtpToJmsAnnotationRoute extends RouteBuilder {
+public class FtpToJmsAnnotationRoute extends SpringRouteBuilder {
 
   private static final String FTP_URI = "file:src/data?noop=true";
   private static final String JMS_URI = "jms:queue:incomingOrders";
