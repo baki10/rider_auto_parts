01db6a54d92758c91831cce3b2faf783726c34ab
Ilmir Bakirov <ilmirbakirov@gmail.com>
2016-10-04 13:54:36 +0300

    annotation component scan

--- src/main/java/com/bakigoal/DownloadLoggerProcessor.java
+++ src/main/java/com/bakigoal/DownloadLoggerProcessor.java
@@ -2,10 +2,12 @@ package com.bakigoal;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by ilmir on 04.10.16.
  */
+@Component
 public class DownloadLoggerProcessor implements Processor {
   public void process(Exchange exchange) throws Exception {
     System.out.println("From: \t" + exchange.getIn());
